5f6096d1cc37effbbedbacdce7e19ab3441fb0e1
Diego Goulart <diegogoulart.aws@gmail.com>
2021-03-28 23:09:42 -0300

    12 Primeiro componente React

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>forms in React</p>
+
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>forms in React</p>
+
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>forms in React</p>
+
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default RepositoryList;
